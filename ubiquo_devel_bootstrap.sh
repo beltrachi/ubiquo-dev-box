7803c3e628fc913bd2e43e47f2ea4a6030bf2747
vagrant <vagrant@precise32.(none)>
2012-12-16 11:15:57 +0000

    Implemented vagrant guest init script and ubiquo devel setup

---
+++ ubiquo_devel_bootstrap.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Script to initialize an environment to develop ubiquo and it's plugins
+# Requires rvm and git already installed 
+
+UBIQUO_FOLDER=$PWD
+UBIQUO_GEMS_FOLDER="$UBIQUO_FOLDER/gems"
+PROJECT_NAME="PROJECT_NAME" # Replace with script param
+BUNDLER_VERSION="1.2.3" # TODO: Fix ubiquo to not require old 1.1.5 bundler"
+
+# To develop on Ubiquo edge:
+#
+# 1. Create a folder for all this (~/workspace/ubiquo/)
+#
+
+mkdir -p $UBIQUO_FOLDER
+
+# 2. Clone github repo on ~/workspace/ubiquo/gems/ubiquo
+
+mkdir -p $UBIQUO_GEMS_FOLDER
+cd $UBIQUO_GEMS_FOLDER 
+git clone git://github.com/gnuine/ubiquo.git
+
+# 3. Create a Gemfile on ~/workspace/ubiquo/Gemfile with
+BUNDLER_FILE=$UBIQUO_FOLDER/Gemfile
+
+echo "source \"https://rubygems.org\"" >> $BUNDLER_FILE
+echo "gem \"ubiquo\", :path => \"$UBIQUO_GEMS_FOLDER/ubiquo\"" >> $BUNDLER_FILE
+
+# 4. Bundle install on this folder
+cd $UBIQUO_FOLDER
+gem install bundler -v $BUNDLER_VERSION --no-ri --no-rdoc
+bundle install 
+
+# 5. Build a project
+echo "bundle exec ubiquo --edge --devel --rvm --gem-path $UBIQUO_GEMS_FOLDER/ubiquo --clone-gems PROJECT_NAME" 
+
+# 6. Init app -- Optional steps or steps that should be done in ubiquo binary
+echo "
+cd $PROJECT_NAME
+bundle exec rake ubiquo:db:init
+"
+
---
+++ vagrant_guest_init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+TARGET_RUBY_VERSION="ruby-1.9.3-p327"
+sudo apt-get -y update
+sudo apt-get -y install build-essential zlib1g-dev git-core sqlite3 libsqlite3-dev curl
+
+curl -L https://get.rvm.io | bash -s stable
+
+echo "source /home/vagrant/.rvm/scripts/rvm " >> ~/.bashrc
+
+source /home/vagrant/.rvm/scripts/rvm
+
+rvm install $TARGET_RUBY_VERSION # Update version when required
+
+rvm --default use $TARGET_RUBY_VERSION
+
+
