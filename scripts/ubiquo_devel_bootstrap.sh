0d8f9927f251c238134619e89d396977ce99b8ea
Jordi Beltran <beltrachi@gmail.com>
2012-12-28 23:08:06 +0100

    Remove bundler version change. Ubiquo has to have a wider bundler version support

--- scripts/ubiquo_devel_bootstrap.sh
+++ scripts/ubiquo_devel_bootstrap.sh
@@ -5,7 +5,6 @@
 UBIQUO_FOLDER=$PWD
 UBIQUO_GEMS_FOLDER="$UBIQUO_FOLDER/gems"
 PROJECT_NAME="PROJECT_NAME" # Replace with script param
-BUNDLER_VERSION="1.2.3" # TODO: Fix ubiquo to not require old 1.1.5 bundler"
 UBIQUO_REPO="git://github.com/gnuine/ubiquo.git"
 
 # To develop on Ubiquo edge:
@@ -29,7 +28,6 @@ echo "gem \"ubiquo\", :path => \"$UBIQUO_GEMS_FOLDER/ubiquo\"" >> $BUNDLER_FILE
 
 # 4. Bundle install on this folder
 cd $UBIQUO_FOLDER
-gem install bundler -v $BUNDLER_VERSION --no-ri --no-rdoc
 bundle install 
 
 # 5. Build a project
